48d30c9d3271ad37ee2a872eb76d64815bd2a76f
irshadi049@gmail.com <irshadi049@gmail.com>
2019-10-26 00:14:28 +0530

    fist comit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+      title: "Flutter for web",
+      
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App bar")
+      ),
+      body: Container(
+        child: Text("welcome"),
+      ),
+      
+    );
+  }
+}
